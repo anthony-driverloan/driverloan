d0278d5ab704466b2ec75a409c3db7a06687d80f
anthony <anthony@driverloan.co.uk>
2015-07-08 22:10:06 +0100

    update

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -21,6 +21,27 @@ angular.module('driverloanV1App')
     });
 
 
+    // if you want to use the 'fire' or 'disable' fn,
+    // you need to save OuiBounce to an object
+    var _ouibounce = ouibounce(document.getElementById('ouibounce-modal'), {
+      aggressive: true,
+      timer: 0,
+      callback: function() { console.log('hi') }
+    });
+
+    $('body').on('click', function() {
+      $('#ouibounce-modal').hide();
+    });
+
+    $('#ouibounce-modal .modal-footer').on('click', function() {
+      $('#ouibounce-modal').hide();
+    });
+
+    $('#ouibounce-modal .modal').on('click', function(e) {
+      e.stopPropagation();
+    });
+
+
 
 
   });
