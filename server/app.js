1b1f122a5c8044cd218628a40666a1f2f9b5bed7
anthony <anthony@driverloan.co.uk>
2015-07-09 22:04:35 +0100

    SSL certificate added

--- server/app.js
+++ server/app.js
@@ -19,8 +19,21 @@ mongoose.connect(config.mongo.uri, config.mongo.options);
 // Populate DB with sample data
 if(config.seedDB) { require('./config/seed'); }
 
+
+//Redirect to https
+function requireHTTPS(req, res, next) {
+  //
+  // The 'x-forwarded-proto' check is for Heroku
+  //
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+};
+
 // Setup server
 var app = express();
+app.use(requireHTTPS);
 app.set('view engine', 'ejs');
 var server = require('http').createServer(app);
 require('./config/express')(app);
